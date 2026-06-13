5126b2553ff039b455dc2d731463357bf63eecb9
ayushpalitt <palitayush1234@gmail.com>
2026-06-13 20:09:59 +0530

    fix: final build fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@/app/globals.css";
+import "./globals.css";
 import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
